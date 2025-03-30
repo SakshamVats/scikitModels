ee9db50d75181049350705cc7be5383336b98764
Saksham Vats <saksham.vats@outlook.com>
2025-03-30 23:15:39 +0530

    Update logistic_regression.py
    Added logistic regression model on UCI Wine Quality dataset

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,1 +1,40 @@
+from cgi import test
+from statistics import LinearRegression
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+df = pd.read_csv(url, sep=";")
+#print(df.head())
+
+GOOD_WINE_THRESHOLD = 7
+df["quality"] = df["quality"].apply(lambda x: 1 if x>= GOOD_WINE_THRESHOLD else 0)
+
+#print(df["quality"].value_counts())
+
+X = df.drop(columns=["quality"])
+y = df["quality"
+       ]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=69)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+print(classification_report(y_test, y_pred))
+
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(5,4))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["Bad", "Good"], yticklabels=["Bad", "Good"])
+plt.xlabel("Predicted Label")
+plt.ylabel("Actual Label")
+plt.title("Confusion Matrix")
+plt.show()
